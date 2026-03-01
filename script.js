e177115a47aff83d7d5b8b8a90c9e6fa471d06b6
Antoine Jamann <144372299+AntoineJamann@users.noreply.github.com>
2026-03-01 11:22:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,4 @@ const message = document.getElementById('message');
 
 heart.addEventListener('click', () => {
     message.style.display = 'block';
-    setTimeout(() => {
-        message.style.display = 'none';
-    }, 3000); // Le message disparaît après 3 secondes
 });
