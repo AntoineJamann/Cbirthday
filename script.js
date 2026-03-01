c1df5eb434455b90ca78e95c726a7f39be813257
Antoine Jamann <144372299+AntoineJamann@users.noreply.github.com>
2026-03-01 13:22:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ document.addEventListener('DOMContentLoaded', () => {
         const dy = e.clientY - heartCenterY;
         const distance = Math.sqrt(dx * dx + dy * dy);
 
-        if (distance < 300) {
+        if (distance < 350) {
             const angle = Math.atan2(dy, dx);
-            const moveX = Math.cos(angle) * 50;
-            const moveY = Math.sin(angle) * 50;
+            const moveX = Math.cos(angle) * 30;
+            const moveY = Math.sin(angle) * 30;
             heart.style.transform = `translate(${moveX - 50}%, ${moveY}px)`;
         } else {
             heart.style.transform = 'translateX(-50%)';
